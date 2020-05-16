d8d954308715880fe1806fb7e404508ba01f5e62
pabloafer10 <pabloafer10@hotmail.com>
2020-05-15 21:57:56 -0300

    Adicionar arquivos de projeto.

---
+++ Carro.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarroCorrigido
+{
+    class Carro
+    {
+        private long id;
+        private double valorDeDiaria;
+        private double valorDeCusto;
+        private string modelo;
+        private string cor;
+        private int ano;
+        private int numDias;
+        private double porcentagem;
+
+        public int NumDias { get => numDias; set => numDias = value; }
+        public double Porcentagem { get => porcentagem; set => porcentagem = value; }
+
+        public Carro(string modelo, string cor, int ano, double valorDeCusto, double valorDeDiaria)
+        {
+            this.modelo = modelo;
+            this.cor = cor;
+            this.ano = ano;
+            this.valorDeCusto = valorDeCusto;
+            this.valorDeDiaria = valorDeDiaria;
+
+        }
+        /* public long Id 
+        {
+            get { return this.id; }
+            set { this.id = this.id; }
+        }
+        public double ValorDeVenda
+        {
+            get { return this.valorDeVenda; }
+            set { this.ValorDeVenda = valorDeCusto * 1.35; }
+        }
+        public double ValorDeDiaria 
+        { 
+            get { return this.valorDeDiaria; }
+            set { this.ValorDeDiaria = this.ValorDeVenda / 200; }
+        }
+        public double ValorDeCusto 
+        { 
+            get { return this.valorDeCusto; } 
+            set { this.ValorDeCusto = this.ValorDeCusto; } 
+        }*/
+
+        public void setId(long id)
+        {
+            this.id = id;
+        }
+        public long getId()
+        {
+            return this.id;
+        }
+        /*public double calcValorDeVenda(double lucro)
+        {
+        return valorDeCusto * lucro;
+        }*/
+        public void setValorDeDiaria(Double vl)
+        {
+            vl = this.valorDeDiaria * NumDias;
+        }
+        public double getValorDeDiaria()
+        {
+            return this.valorDeDiaria;
+        }
+        public void setValorDeCusto(double vC)
+        {
+            vC = this.valorDeCusto * Porcentagem;
+        }
+        public double getValorDeCusto()
+        {
+            return this.valorDeCusto;
+        }
+        public void setModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public string getModelo()
+        {
+            return this.modelo;
+        }
+        public void setCor(string cor)
+        {
+            this.cor = cor;
+        }
+        public string getCor()
+        {
+            return this.cor;
+        }
+        public void setAno(int ano)
+        {
+            this.ano = ano;
+        }
+        public int getAno()
+        {
+            return this.ano;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace CarroCorrigido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro car1 = new Carro("Renault Kwid Life", "Vermelho", 2018, 16000, 80);
+            Carro car2 = new Carro("Hyundai HB20 Sense", "Branco", 2019, 17000, 100);
+            Carro car3 = new Carro("Ford Ka S", "Preto", 2017, 15000, 50);
+            Carro car4 = new Carro("Volkswagen Gol", "Prata", 2018, 20000, 80);
+            Carro car5 = new Carro("Chevrolet Joy", "Amarelo", 2020, 23000, 130);
+
+            string menu;
+            int op;
+            double valor;
+            double porcentagem;
+            double vV;
+            int qtdeDias;
+            double vL;
+
+            menu = "*******************************************************\nLOCAVENDAS VEICULOS\n*******************************************************\n1.Listar Carros.\n2.Vendas.\n3.Locação.\n0.Sair.\n*******************************************************";
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(menu);
+                Console.Write("\n Opção: ");
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.Write("********************************************************\nListar Carros\n********************************************************");
+                        Console.Write("\nCARROS                  COR       ANO    CUSTO   DIÁRIA\n********************************************************");
+                        Console.Write("\n1." + car1.getModelo() + "   " + car1.getCor() + "    " + car1.getAno() + "   " + car1.getValorDeCusto() + "     " + car1.getValorDeDiaria() + "" +
+                                      "\n2." + car2.getModelo() + "   " + car2.getCor() + "     " + car2.getAno() + "   " + car2.getValorDeCusto() + "    " + car2.getValorDeDiaria() + "" +
+                                      "\n3." + car3.getModelo() + "             " + car3.getCor() + "     " + car3.getAno() + "   " + car3.getValorDeCusto() + "     " + car3.getValorDeDiaria() + "" +
+                                      "\n4." + car4.getModelo() + "        " + car4.getCor() + "     " + car4.getAno() + "   " + car4.getValorDeCusto() + "     " + car4.getValorDeDiaria() + "" +
+                                      "\n5." + car5.getModelo() + "        " + car5.getCor() + "    " + car5.getAno() + "   " + car5.getValorDeCusto() + "    " + car5.getValorDeDiaria() + "");
+                        break;
+
+                    case 2:
+                        Console.Write("********************************************************\nVender\n********************************************************");
+                        Console.Write("\nVenda de Carros\n********************************************************");
+                        Console.WriteLine("\nDigite o id do carro: ");// COMO PODERIA FAZER PARA CHAMAR O NOME DO CARRO SEM NECESSIDADE DE DIGITAR O NOME
+                        Console.Write("\nOpção: ");
+                        op = Convert.ToInt32(Console.ReadLine());
+                        switch (op)
+                        {
+                            case 1:
+                                Console.WriteLine("1 - {0} Custo{1} " , car1.getModelo(), car1.getValorDeCusto());
+                                valor = car1.getValorDeCusto();
+                                Console.Write("\nDigite o percentual de lucro : ");
+                                porcentagem = Convert.ToDouble(Console.ReadLine());
+                                vV = valor * porcentagem;
+                                Console.WriteLine("\nO valor da venda é R$: " + vV + "% .2f");
+                                Console.ReadLine();
+                                break;
+
+                            case 2:
+                                Console.WriteLine("C2 - {0} Custo{1} " , car2.getModelo(), car2.getValorDeCusto());
+                                valor = car2.getValorDeCusto();
+                                Console.Write("\nDigite o percentual de lucro : ");
+                                porcentagem = Convert.ToDouble(Console.ReadLine());
+                                vV = valor * porcentagem;
+                                Console.WriteLine("\nO valor da venda é R$: " + vV + "% .2f");
+                                Console.ReadLine();
+                                break;
+
+                            case 3:
+                                Console.WriteLine("3 - {0} Custo{1} " , car3.getModelo(), car3.getValorDeCusto());
+                                valor = car3.getValorDeCusto();
+                                Console.Write("\nDigite o percentual de lucro : ");
+                                porcentagem = Convert.ToDouble(Console.ReadLine());
+                                vV = valor * porcentagem;
+                                Console.WriteLine("\nO valor da venda é R$: " + vV + "% .2f");
+                                Console.ReadLine();
+                                break;
+
+                            case 4:
+                                Console.WriteLine("4 - {0} Custo{1} " , car4.getModelo(), car4.getValorDeCusto());
+                                valor = car4.getValorDeCusto();
+                                Console.Write("\nDigite o percentual de lucro : ");
+                                porcentagem = Convert.ToDouble(Console.ReadLine());
+                                vV = valor * porcentagem;
+                                Console.WriteLine("\nO valor da venda é R$: " + vV + "% .2f");
+                                Console.ReadLine();
+                                break;
+
+
+                            case 5:
+                                Console.WriteLine("5 - {0} Custo{1} " , car5.getModelo(), car5.getValorDeCusto());
+                                valor = car5.getValorDeCusto();
+                                Console.Write("\nDigite o percentual de lucro : ");
+                                porcentagem = Convert.ToDouble(Console.ReadLine());
+                                vV = valor * porcentagem;
+                                Console.WriteLine("\nO valor da venda é R$: " + vV + "% .2f");
+                                Console.ReadLine();
+                                break;
+
+                        }
+                        break;
+
+                    case 3:
+                        Console.Write("********************************************************\nLocar\n********************************************************");
+                        Console.Write("\nLocar Carros\n********************************************************");
+                        Console.WriteLine("\nDigite o id do carro: ");// COMO PODERIA FAZER PARA CHAMAR O NOME DO CARRO SEM NECESSIDADE DE DIGITAR O NOME
+                        Console.Write("\nOpção: ");
+                        op = Convert.ToInt32(Console.ReadLine());
+                        switch (op)
+                        {
+                            case 1:
+                                Console.WriteLine("1 - {0} Custo{1} " , car1.getModelo(), car1.getValorDeDiaria());
+                                valor = car1.getValorDeDiaria();
+                                Console.Write("\nDigite a quantidade de dias: ");
+                                qtdeDias = Convert.ToInt32(Console.ReadLine());
+                                vL = valor * qtdeDias;
+                                Console.WriteLine("\nO valor da locação é R$: " + vL);
+                                Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.WriteLine("2 - {0} Custo{1} " , car2.getModelo(), car2.getValorDeDiaria());
+                                valor = car2.getValorDeDiaria();
+                                Console.Write("\nDigite a quantidade de dias: ");
+                                qtdeDias = Convert.ToInt32(Console.ReadLine());
+                                vL = valor * qtdeDias;
+                                Console.WriteLine("\nO valor da locação é R$: " + vL);
+                                Console.ReadLine();
+                                break;
+                            case 3:
+                                Console.WriteLine("3 - {0} Custo{1} " , car3.getModelo(), car3.getValorDeDiaria());
+                                valor = car3.getValorDeDiaria();
+                                Console.Write("\nDigite a quantidade de dias: ");
+                                qtdeDias = Convert.ToInt32(Console.ReadLine());
+                                vL = valor * qtdeDias;
+                                Console.WriteLine("\nO valor da locação é R$: " + vL);
+                                Console.ReadLine();
+                                break;
+                            case 4:
+                                Console.WriteLine("4 - {0} Custo{1} " , car4.getModelo(), car4.getValorDeDiaria());
+                                valor = car4.getValorDeDiaria();
+                                Console.Write("\nDigite a quantidade de dias: ");
+                                qtdeDias = Convert.ToInt32(Console.ReadLine());
+                                vL = valor * qtdeDias;
+                                Console.WriteLine("\nO valor da locação é R$: " + vL);
+                                Console.ReadLine();
+                                break;
+                            case 5:
+                                Console.WriteLine("5 - {0} Custo{1} " , car5.getModelo(), car5.getValorDeDiaria());
+                                valor = car5.getValorDeDiaria();
+                                Console.Write("\nDigite a quantidade de dias: ");
+                                qtdeDias = Convert.ToInt32(Console.ReadLine());
+                                vL = valor * qtdeDias;
+                                Console.WriteLine("\nO valor da locação é R$: " + vL);
+                                Console.ReadLine();
+                                break;
+                        }
+                            break;
+                            case 0:
+                        Console.Write("\nAté Breve!");
+                        break;
+                    default:
+                        Console.Write("\n Opção inválida, digite novamente! ");
+                        break;
+                }
+                Console.ReadKey();
+            } while (op != 0);
+
+        }
+    }
+}
